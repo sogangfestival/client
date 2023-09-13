9ef43b3c5f371fc4056b6f856c7c3105d22c4c5e
NakedGreen <jokemup@gmail.com>
2023-09-14 02:39:49 +0900

    feat/map

---
+++ src/utils/maindata.js
@@ -0,0 +1,282 @@
+export const mapData = [
+  {
+    name: "#정문",
+    coord: {
+      x: 54.84,
+      y: 136.32,
+    },
+  },
+  {
+    name: "#엠마오관",
+    coord: {
+      x: 197,
+      y: 74,
+    },
+  },
+  {
+    name: "#대운동장",
+    coord: {
+      x: 231.36,
+      y: 85.16,
+    },
+  },
+
+  {
+    name: "#알바탑",
+    coord: {
+      x: 106,
+      y: 120,
+    },
+  },
+  {
+    name: "#로욜라도서관",
+    coord: {
+      x: 200,
+      y: 51,
+    },
+  },
+  {
+    name: "#AS관",
+    coord: {
+      x: 284,
+      y: 78,
+    },
+  },
+  {
+    name: "#본관",
+    coord: {
+      x: 175,
+      y: 89,
+    },
+  },
+  {
+    name: "#CY관",
+    coord: {
+      x: 240,
+      y: 58,
+    },
+  },
+
+  {
+    name: "#R관",
+    coord: {
+      x: 268,
+      y: 100,
+    },
+  },
+  {
+    name: "#GN관",
+    coord: {
+      x: 199,
+      y: 1221,
+    },
+  },
+  {
+    name: "#X관",
+    coord: {
+      x: 234,
+      y: 44,
+    },
+  },
+  {
+    name: "#예수회센터",
+    coord: {
+      x: 257,
+      y: 143,
+    },
+  },
+  {
+    name: "#예수회공동체",
+    coord: {
+      x: 129,
+      y: 57,
+    },
+  },
+  {
+    name: "#D관",
+    coord: {
+      x: 217,
+      y: 24,
+    },
+  },
+  {
+    name: "#K관",
+    coord: {
+      x: 229,
+      y: 130,
+    },
+  },
+  {
+    name: "#GA관",
+    coord: {
+      x: 94.95,
+      y: 84.16,
+    },
+  },
+  {
+    name: "#곤자가",
+    coord: {
+      x: 263,
+      y: 29,
+    },
+  },
+  {
+    name: "#벨라르미노",
+    coord: {
+      x: 285,
+      y: 163,
+    },
+  },
+  {
+    name: "#PA관",
+    coord: {
+      x: 76,
+      y: 74,
+    },
+  },
+  {
+    name: "#후문",
+    coord: {
+      x: 294,
+      y: 43,
+    },
+  },
+  {
+    name: "#서강빌딩",
+    coord: {
+      x: 234,
+      y: 178,
+    },
+  },
+  {
+    name: "#T관",
+    coord: {
+      x: 41,
+      y: 107,
+    },
+  },
+  {
+    name: "#곤자가플라자",
+    coord: {
+      x: 257,
+      y: 55,
+    },
+  },
+  {
+    name: "#남문",
+    coord: {
+      x: 216,
+      y: 202,
+    },
+  },
+  {
+    name: "#MA관",
+    coord: {
+      x: 78.25,
+      y: 60.37,
+    },
+  },
+  {
+    name: "#TE관",
+    coord: {
+      x: 312,
+      y: 40,
+    },
+  },
+  {
+    name: "#아루페관",
+    coord: {
+      x: 171,
+      y: 161,
+    },
+  },
+  {
+    name: "#메리홀",
+    coord: {
+      x: 106,
+      y: 72,
+    },
+  },
+  {
+    name: "#J관",
+    coord: {
+      x: 304,
+      y: 51,
+    },
+  },
+  {
+    name: "#체육관",
+    coord: {
+      x: 203,
+      y: 157,
+    },
+  },
+  {
+    name: "#I관",
+    coord: {
+      x: 129,
+      y: 57,
+    },
+  },
+  {
+    name: "#F관",
+    coord: {
+      x: 300,
+      y: 57,
+    },
+  },
+  {
+    name: "#청년광장",
+    coord: {
+      x: 128.36,
+      y: 107.19,
+    },
+  },
+  {
+    name: "#로욜라동산",
+    coord: {
+      x: 179,
+      y: 56,
+    },
+  },
+  {
+    name: "#RA관",
+    coord: {
+      x: 295,
+      y: 64,
+    },
+  },
+  {
+    name: "#우정원",
+    coord: {
+      x: 155,
+      y: 141,
+    },
+  },
+];
+
+export const blueData = [
+  { building: "엠마오관", name: "팀이름", money: "₩ 50만" },
+  { building: "우정원", name: "팀이름", money: "₩ 50만" },
+  { building: "GN관", name: "팀이름", money: "₩ 50만" },
+  { building: "R관", name: "팀이름", money: "₩ 50만" },
+  { building: "AS관", name: "팀이름", money: "₩ 50만" },
+  { building: "J관", name: "팀이름", money: "₩ 50만" },
+  { building: "X관", name: "팀이름", money: "₩ 50만" },
+  { building: "D관", name: "팀이름", money: "₩ 50만" },
+  { building: "GA관", name: "팀이름", money: "₩ 50만" },
+  { building: "MA관", name: "팀이름", money: "₩ 50만" },
+  { building: "체육관", name: "팀이름", money: "₩ 50만" },
+  { building: "AR관", name: "팀이름", money: "₩ 50만" },
+  { building: "청년광장", name: "팀이름", money: "₩ 50만" },
+  { building: "토마스모어관", name: "팀이름", money: "₩ 50만" },
+  { building: "PA관", name: "팀이름", money: "₩ 50만" },
+  { building: "I관", name: "팀이름", money: "₩ 50만" },
+  { building: "본관", name: "팀이름", money: "₩ 50만" },
+  { building: "로욜라", name: "팀이름", money: "₩ 50만" },
+  { building: "곤플", name: "팀이름", money: "₩ 50만" },
+  { building: "TE관", name: "팀이름", money: "₩ 50만" },
+  { building: "F관", name: "팀이름", money: "₩ 50만" },
+  { building: "RA관", name: "팀이름", money: "₩ 50만" },
+  { building: "K관", name: "팀이름", money: "₩ 50만" },
+];
