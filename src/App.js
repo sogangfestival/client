0b8c498f22ac19180b27b4bca864f3e95132fd8c
wooseok123 <hosmimam@naver.com>
2023-09-14 03:31:32 +0900

    seo 처리

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import RouterComponent from "Router";
 import AppContainer from "@components/templates/AppContainer";
 import GlobalStyle from "@styles/GlobalStyle";
 import "@styles/font.css";
-import Header from "@components/organisms/Header";
+import MetaTag from "SEO";
+import { config } from "@utils/SEOConfig";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <AppContainer>
+        <MetaTag {...config} />
         <RouterComponent />
       </AppContainer>
     </>
---
+++ src/utils/SEOConfig.js
@@ -0,0 +1,7 @@
+export const config = {
+  description: "2023 CARDINAL : 응답하라 서강",
+  keywords: "서강대학교, 축제, CARDINAL",
+  title: "2023 CARDINAL : 응답하라 서강",
+  imgsrc: "/src/assets/logo.png",
+  url: "https://www.sgucardinal.com",
+};
