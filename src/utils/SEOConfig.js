d11324e02d5ca9d0f20f21c9306a6304fafcca96
wooseok123 <hosmimam@naver.com>
2023-09-14 03:36:24 +0900

    이미지 경로 변경

--- src/utils/SEOConfig.js
+++ src/utils/SEOConfig.js
@@ -1,7 +1,9 @@
+import logo from "@assets/logo.png";
+
 export const config = {
   description: "2023 CARDINAL : 응답하라 서강",
   keywords: "서강대학교, 축제, CARDINAL",
   title: "2023 CARDINAL : 응답하라 서강",
-  imgsrc: "/src/assets/logo.png",
+  imgsrc: logo,
   url: "https://www.sgucardinal.com",
 };
