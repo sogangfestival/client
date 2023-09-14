cc1a4df2ca1bfc5676997c123a873eef7f3aeffd
wooseok123 <hosmimam@naver.com>
2023-09-14 14:38:42 +0900

    seo 수정

--- src/SEO.js
+++ src/SEO.js
@@ -7,22 +7,22 @@ const MetaTag = () => {
   return (
     <Helmet>
       <title>2023 CARDINAL : 응답하라 서강</title>
-      <meta itemProp="keywords" content="분실물" />
-      <meta itemProp="name" content={config?.title} />
-      <meta itemProp="description" content={config?.description} />
-      <meta itemProp="image" content={config?.imgsrc} />
+      <meta property="keywords" content="분실물" />
+      <meta property="name" content={config?.title} />
+      <meta property="description" content={config?.description} />
+      <meta property="image" content={config?.imgsrc} />
       <meta property="og:site_name" content={config?.title} />
-      <meta itemProp="og:url" content="https://www.sgucardinal.com" />
+      <meta property="og:url" content="https://www.sgucardinal.com" />
       <meta property="og:title" content={"2023 CARDINAL : 응답하라 서강"} />
       <link rel="canonical" href="https://www.sgucardinal.com" />
-      <meta itemProp="og:image" content={config?.imgsrc || logo} />
+      <meta property="og:image" content={config?.imgsrc || logo} />
       <meta
-        itemProp="og:description"
+        property="og:description"
         content={"2023 CARDINAL : 응답하라 서강"}
       />
-      <meta itemProp="twitter:title" content={config?.title} />
-      <meta itemProp="twitter:description" content={config?.description} />
-      <meta itemProp="twitter:image" content={config?.imgsrc || logo} />
+      <meta property="twitter:title" content={config?.title} />
+      <meta property="twitter:description" content={config?.description} />
+      <meta property="twitter:image" content={config?.imgsrc || logo} />
     </Helmet>
   );
 };
