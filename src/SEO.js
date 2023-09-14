658864b2d159ec54163372002fd571809e3fe3e2
wooseok123 <hosmimam@naver.com>
2023-09-14 14:36:06 +0900

    seo 변경

--- src/SEO.js
+++ src/SEO.js
@@ -13,9 +13,13 @@ const MetaTag = () => {
       <meta itemProp="image" content={config?.imgsrc} />
       <meta property="og:site_name" content={config?.title} />
       <meta itemProp="og:url" content="https://www.sgucardinal.com" />
+      <meta property="og:title" content={"2023 CARDINAL : 응답하라 서강"} />
       <link rel="canonical" href="https://www.sgucardinal.com" />
       <meta itemProp="og:image" content={config?.imgsrc || logo} />
-      <meta itemProp="og:description" content={config?.description} />
+      <meta
+        itemProp="og:description"
+        content={"2023 CARDINAL : 응답하라 서강"}
+      />
       <meta itemProp="twitter:title" content={config?.title} />
       <meta itemProp="twitter:description" content={config?.description} />
       <meta itemProp="twitter:image" content={config?.imgsrc || logo} />
