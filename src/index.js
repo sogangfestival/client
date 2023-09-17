80a6eec74198b715ea7641c8138bb9590354b8ee
wooseok123 <hosmimam@naver.com>
2023-09-18 02:59:16 +0900

    seo 처리

--- src/index.js
+++ src/index.js
@@ -6,21 +6,17 @@ import { hydrate, render } from "react-dom";
 
 const root = document.getElementById("root");
 if (root.hasChildNodes()) {
-  if (navigator.userAgent !== "ReactSnap") {
-    hydrate(
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>,
-      root
-    );
-  }
+  hydrate(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>,
+    root
+  );
 } else {
-  if (navigator.userAgent !== "ReactSnap") {
-    render(
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>,
-      root
-    );
-  }
+  render(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>,
+    root
+  );
 }
