a03fcd145f823f433cd1ba3c9ea2289773583290
wooseok123 <hosmimam@naver.com>
2023-09-18 17:55:47 +0900

    svg to png

--- src/components/molecules/Back/index.js
+++ src/components/molecules/Back/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import left from "@assets/leftPage.svg";
+import left from "@assets/leftPage.png";
 import { useNavigate } from "react-router-dom";
 
 const Back = () => {
@@ -7,11 +7,10 @@ const Back = () => {
 
   return (
     <>
-      <object
+      <img
         onClick={() => navigate(-1)}
         style={{ cursor: "pointer", width: "11px", height: "18px" }}
-        data={left}
-        type="image/svg+xml"
+        src={left}
       />
     </>
   );
