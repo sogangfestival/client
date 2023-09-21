f9b77b80d829542f3a378916f48683f234cb7f9a
wooseok123 <hosmimam@naver.com>
2023-09-21 10:34:38 +0900

    console.log() 사겢

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -43,7 +43,6 @@ const Map = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [buttonClicked, setButtonClicked] = useState({});
   const [selectedButtons, setSelectedButtons] = useState([]);
-  console.log(selectedButtons);
   const [toggleSwitch, setToggleSwitch] = useState("학교");
   const menuRef = useRef(null);
 
